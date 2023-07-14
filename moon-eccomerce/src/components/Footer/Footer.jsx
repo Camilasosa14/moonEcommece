f5e1609f3bae6650d954a8022787d3fa18a75dd4
Camila Sosa <camilasosa1997@gmail.com>
2023-07-14 19:32:09 -0300

    links footer

--- moon-eccomerce/src/components/Footer/Footer.jsx
+++ moon-eccomerce/src/components/Footer/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <div className="footerContainer">
             <p>Seguinos en nuestra redes</p>
             
-            <a href="*"><FaWhatsapp/></a>
-            <a href="*"><FaInstagram/></a>
-            <a href="*"><FaFacebook/></a>
-            <a href="*"><FaTiktok/></a>
+            <a href="https://www.coderhouse.com/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gclid=CjwKCAjw5MOlBhBTEiwAAJ8e1j822mpXnHycVWDA_OL4kdAnl-yLi5CMrX1PLKUosrrjkic3c5Gh8xoCf6MQAvD_BwE"><FaWhatsapp/></a>
+            <a href="https://www.coderhouse.com/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gclid=CjwKCAjw5MOlBhBTEiwAAJ8e1j822mpXnHycVWDA_OL4kdAnl-yLi5CMrX1PLKUosrrjkic3c5Gh8xoCf6MQAvD_BwE"><FaInstagram/></a>
+            <a href="https://www.coderhouse.com/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gclid=CjwKCAjw5MOlBhBTEiwAAJ8e1j822mpXnHycVWDA_OL4kdAnl-yLi5CMrX1PLKUosrrjkic3c5Gh8xoCf6MQAvD_BwE"><FaFacebook/></a>
+            <a href="https://www.coderhouse.com/?pipe_source=google&pipe_medium=cpc&pipe_campaign=0&gclid=CjwKCAjw5MOlBhBTEiwAAJ8e1j822mpXnHycVWDA_OL4kdAnl-yLi5CMrX1PLKUosrrjkic3c5Gh8xoCf6MQAvD_BwE"><FaTiktok/></a>
         </div>
     )
 }
